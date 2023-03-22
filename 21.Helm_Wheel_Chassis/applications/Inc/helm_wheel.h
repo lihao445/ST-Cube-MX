50dad46b03ce039d2886de0ff566b5192f1b3c30
tungchiahui <tungchia-hui@foxmail.com>
2023-03-22 17:27:28 +0800

    tung

--- 21.Helm_Wheel_Chassis/applications/Inc/helm_wheel.h
+++ 21.Helm_Wheel_Chassis/applications/Inc/helm_wheel.h
@@ -6,8 +6,7 @@ float calc_angle_helm(float set_ch2,float set_ch3);
 
 float calc_motor_round_cnt(float angle,float last_angle);
 
-
-
+float calc_min_angle(float set_angle,float last_set_angle);
 
 #endif
 
